8e00917a0f7fde968568f1067596116f89e31485
yakubu234 <yakubuabiola2003@gmail.com>
2022-08-16 11:50:27 +0100

    adding allow remote origin

--- csrf_scraper.php
+++ csrf_scraper.php
@@ -1,4 +1,24 @@
 <?php
+
+// Allow from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    // should do a check here to match $_SERVER['HTTP_ORIGIN'] to a
+    // whitelist of safe domains
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+}
+
+
 #create a laravel app with crud requirements, host on my personal server and also host scrapper on the personal server, and the UI on a personal servver
 # get at the form input tag <input type="hidden" name="_token" value="t0l2x6ggZmd06aL8Mo4Wv6bQPQOAP0vPYZlyBjFK">
 function is_base64($string)
